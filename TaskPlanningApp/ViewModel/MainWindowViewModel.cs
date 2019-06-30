eb6b773ba62124594ea3634be116df8e42be683b
mkopacz94 <m.kopacz94@gmail.com>
2019-06-30 19:59:47 +0200

    Started MVVM approach.

---
+++ TaskPlanningApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace TaskPlanningApp.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public RelayCommand<IClosable> CloseCommand {get; set;}
+
+        public MainWindowViewModel()
+        {
+            this.CloseCommand = new RelayCommand<IClosable>(this.CloseWindow);
+        }
+
+        private void CloseWindow(IClosable window)
+        {
+            if (window != null)
+                window.Close();
+        }
+    }
+
+}
